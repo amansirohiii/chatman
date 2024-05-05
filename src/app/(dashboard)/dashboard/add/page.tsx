28caa00f45a6918b7068d190f242dc94839379c3
Aman Sirohi <amansirohi077@gmail.com>
2024-05-05 19:55:18 +0530

    added add friend page

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,12 @@
+import AddFriendButton from "@/components/AddFriendButton"
+import React from 'react'
+
+const AddFriend = () => {
+  return (
+<main className='pt-8'>
+    <h1 className="font-bold text-5xl mb-8"> Add a friend</h1>
+    <AddFriendButton />
+</main>  )
+}
+
+export default AddFriend
